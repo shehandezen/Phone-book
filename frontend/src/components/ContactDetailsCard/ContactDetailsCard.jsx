63013aaec351ee402203f978d54307b25869b658
shehandezen <117962951+shehandezen@users.noreply.github.com>
2023-07-11 06:06:13 +0000

    fix some mistakes

--- frontend/src/components/ContactDetailsCard/ContactDetailsCard.jsx
+++ frontend/src/components/ContactDetailsCard/ContactDetailsCard.jsx
@@ -14,6 +14,7 @@ import "./ContactDetailsCard.css";
 import Avatar from "../../assets/avatar.png";
 
 export const ContactDetailsCard = () => {
+  axios.defaults.withCredentials = true;
   const navigate = useNavigate();
   const [data, setData] = useState({
     prefix: "",
